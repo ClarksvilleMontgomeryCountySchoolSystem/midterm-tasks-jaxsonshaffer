2e3a9f6cc0103ffa0952cdf37c08930f1db32be8
jaxsonshaffer <jaxson.shaffer369@students.cmcss.net>
2025-12-18 13:55:21 -0600

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,1 +1,23 @@
+slices = party_pizza_mini+large+medium
+print(f"{slices}")
 
+people += 1
+share = slices//people
+leftover = slices%people
+print(f"Each person gets: {share}")
+print(f"leftover slices: {leftover}")
+
+people += 2 #Eric and Brandon are coming too.
+share = slices//people
+leftover = slices%people
+print(f"Each person gets: {share}")
+print(f"Leftover slices; {leftover}")
+
+
+
+slices += party_pizza_mini
+share = slices//people
+leftover = slices%people
+print(f"Each person gets: {share}")
+print(f"Leftover slices: {leftover}")
+print("...for Mr. Hollow Leg")
